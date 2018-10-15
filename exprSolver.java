8c6478e1c75ab975c30a34ca8434de4d83787464
Luana Porciuncula Barreto <lpb@ug1c23.windows.cin.ufpe.br>
2018-10-15 12:05:35 -0300

    Agr ta lendo as entrada lokona la mas fiz nada ainda nnnnnn

--- exprSolver.java
+++ exprSolver.java
@@ -8,35 +8,66 @@ public class exprSolver {
 			FileWriter out = new FileWriter("Saída.out");
 
 			String linha = in.nextLine();
+			int num_problem = 1;
 
 			int qtd_prob = Integer.parseInt(linha);
 
-			while (qtd_prob > 0) {
-				String expr;
-				String strValues;
+			while (num_problem <= 3) {
+				String expr, strValues;
 				String[] values;
+				int indexEnd = 0;
 				
 				linha = in.nextLine();
-				int i = 0;
-				for (; linha.charAt(i) != '0' && linha.charAt(i) != '1' 
-					&& i < linha.length(); i++) {
-				}
-				expr = linha.substring(0, i-1);
-				strValues = linha.substring(i, linha.length());
+				out.write("Problema #"+num_problem+"\n");
+				System.out.print("Problema #"+num_problem+"\n");
+
+				for (; indexEnd < linha.length() && linha.charAt(indexEnd) != '0' && linha.charAt(indexEnd) != '1'; indexEnd++) {}
+
+				expr = linha.substring(0, indexEnd-1);
+				strValues = linha.substring(indexEnd, linha.length());
 				values = strValues.split(" ");
-				
+				//System.out.println(expr);
+
 				if (linha.charAt(0) == '{') {
-					
+					String[] clauses;
+					boolean valid, sat;
+
+					expr = expr.substring(1, expr.length()-1);
+					clauses = expr.split(",");
+					for(int i = 0; i < clauses.length; i++){
+
+					}
 				} else {
+					if(isValid()){
+						if (valSat()){
+							out.write("A valoracao-verdade satisfaz a proposicao.\n\n");
+							System.out.print("A valoracao-verdade satisfaz a proposicao.\n\n");
+						} else {
+							out.write("A valoracao-verdade nao satisfaz a proposicao.\n\n");
+							System.out.print("A valoracao-verdade nao satisfaz a proposicao.\n\n");
+						}
+					} else {
+						out.write("A palavra nao e legitima.​\n\n");
+						System.out.print("A palavra nao e legitima.​\n\n");
+						
+					}
 				}
 
-				qtd_prob--;
+				num_problem++;
 			}
 
 			out.close();
 			in.close();
-		} catch (IOException e) {
-
-		}
+		} catch (IOException e) {}
 	}
+
+	public static boolean isValid(){
+		return true;
+	}
+
+	public static boolean valSat(){
+		return true;
+	}
+
+
 }
