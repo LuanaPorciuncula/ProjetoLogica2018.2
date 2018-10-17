9736784209f8ab40017d93863d65b01d61e6fe30
Luana Porciuncula Barreto <lpb@ug1c29.windows.cin.ufpe.br>
2018-10-17 15:41:31 -0300

    Corrigida função de checagem de legitimidade, adicionada função que facilita a checagem do valor de uma variável

--- exprSolver.java
+++ exprSolver.java
@@ -13,28 +13,28 @@ public class exprSolver {
 			num_problem = 1;
 			qtd_prob = Integer.parseInt(line);
 
-			while (num_problem <= qtd_prob) {
-				out.write("Problema #" + num_problem + "\r");
+			while (num_problem <= qtd_prob && in.hasNextLine()) {
+				out.write("Problema #" + num_problem + System.lineSeparator());
 
 				String expr, strValues;
 				String[] values;
+				boolean[] alfValues;
 				char[] varList;
 				int indexEnd = 0;
 				
 				line = in.nextLine();
-				//for pra procurar onde acaba a palavra/conjunto e onde começa a valoração
+				//um for pra procurar onde acaba a palavra/conjunto e onde começa a valoração
 				for (; indexEnd < line.length() && line.charAt(indexEnd) != '0' && line.charAt(indexEnd) != '1'; indexEnd++) {}
 
 				expr = line.substring(0, indexEnd-1);
 				strValues = line.substring(indexEnd, line.length());
 				values = strValues.split(" ");
-				/*CRIAR UMA NOVA FUNÇÃO PRA ORGANIZAR OS VALORES NUM ARRAY DE SIZE 26
-					ONDE CADA POSIÇÃO CORRESPONDE A VARIÁVEL*/
 
 				if (line.charAt(0) == '{') {
 
 					varList = listVariables(expr, values.length);
-					String validSat = setAnalysis(expr, varList, values);
+					alfValues = fixValues(varList, values);
+					String validSat = setAnalysis(expr, alfValues);
 
 					if (validSat.charAt(0) == '1'){
 						if(validSat.charAt(1) == '1'){
@@ -48,10 +48,11 @@ public class exprSolver {
 
 				} else {
 
-					if(isValid(expr)){
+					if(legitimacy(expr)){
 						varList = listVariables(expr, values.length);
+						alfValues = fixValues(varList, values);
 
-						if (valSat(expr, varList, values)){
+						if (valuation(expr, alfValues)){
 							out.write("A valoracao-verdade satisfaz a proposicao.");
 						} else {
 							out.write("A valoracao-verdade nao satisfaz a proposicao.");
@@ -62,9 +63,9 @@ public class exprSolver {
 				}
 
 				if(num_problem!=qtd_prob){
-					out.write("\r\r");
+					out.write(System.lineSeparator() + System.lineSeparator());
 				} else {
-					out.write("\r");
+					out.write(System.lineSeparator());
 				}
 				num_problem++;
 			}
@@ -73,17 +74,18 @@ public class exprSolver {
 			in.close();
 		} catch (IOException e) {}
 	}
-
-	public static String setAnalysis(String expr, char[] varList, String[] values){
+	//Função que chama a checagem de legitimidade e valoração de cada expressão do conjunto
+	public static String setAnalysis(String set, boolean[] values){
 		String[] clauses;
 		boolean valid = true, sat = true;
 		String validSat =  "11";
-		expr = expr.substring(1, expr.length()-1);
-		clauses = expr.split(", ");
+		//primeiro digito indica legitimidade e o segundo se a valoração satisfaz o conjunto
+		set = set.substring(1, set.length()-1);
+		clauses = set.split(", ");
 
 		for(int i = 0; i < clauses.length && valid; i++){
-			if(isValid(clauses[i])){
-				if (sat && !valSat(clauses[i], varList, values)){
+			if(legitimacy(clauses[i])){
+				if (sat && !valuation(clauses[i], values)){
 					sat = false;
 					validSat = "10";
 				}
@@ -96,7 +98,8 @@ public class exprSolver {
 		return validSat;
 	}
 
-	public static boolean isValid (String word){
+	//Função que checa a legitimidade de uma expressão de forma recursiva
+	public static boolean legitimacy (String word){
 		int wordLength = word.length();
 
 		//Se a palavra word é atômica
@@ -104,56 +107,49 @@ public class exprSolver {
 			return true;
 		}
 
-		if(word.charAt(0)== '(' && word.charAt(wordLength-1) == ')'){
+		if(wordLength >= 4 && word.charAt(0)== '(' && word.charAt(wordLength-1) == ')'){
 			//Se a palavra word é da forma (~w)
-			if (wordLength >= 4 && word.charAt(1)== '~'){
-				return isValid(word.substring(2, wordLength-1));
+			if (word.charAt(1)== '~'){
+				return legitimacy(word.substring(2, wordLength-1));
 			}
 
 			int indexOp = lastOp(word);
 			//Se a palavra word é da forma (w1 □ w2), onde □ e {&, v, >}
-			if (wordLength >= 7 && indexOp != -1){
-				return isValid(word.substring(1, indexOp - 1)) 
-					&& isValid(word.substring(indexOp + 2, wordLength-1));
+			if (wordLength >= 7 && indexOp != -1 
+					&& word.charAt(indexOp - 1) == ' ' && word.charAt(indexOp + 1) == ' '){
+				return legitimacy(word.substring(1, indexOp - 1)) 
+					&& legitimacy(word.substring(indexOp + 2, wordLength-1));
 			}
 		}
 		return false;
 	}
-
-	public static boolean valSat(String word, char[] varList, String[] values){
+	//Função que verifica se a valoração satisfaz a expressão
+	public static boolean valuation(String word,  boolean[] values){
 		int wordLength = word.length();
 		
 		//Se a palavra word é atômica
 		if (wordLength == 1){
-			for (int i = 0; i < varList.length; i++){
-				if (varList[i] == word.charAt(0)){
-					if (values[i].equals("1")){
-						return true;
-					} else {
-						return false;
-					}
-				}
-			}
+			return values[word.charAt(0) - 65];
 		}
 		
 		//Se a palavra word é da forma (~w)
 		if (word.charAt(1)== '~'){
-			return !valSat(word.substring(2, wordLength-1), varList, values);
+			return !valuation(word.substring(2, wordLength-1), values);
 		}
 		int indexOp = lastOp(word);
 		// Se a palavra word é da forma (w1 & w2)
 		if (word.charAt(indexOp) == '&'){
-			return valSat(word.substring(1, indexOp - 1), varList, values) 
-				&& valSat(word.substring(indexOp + 2, wordLength-1), varList, values);
+			return valuation(word.substring(1, indexOp - 1), values) 
+				&& valuation(word.substring(indexOp + 2, wordLength-1), values);
 		} //Se a palavra word é da forma (w1 v w2)
 		else if (word.charAt(indexOp) == 'v') {
-			return valSat(word.substring(1, indexOp - 1), varList, values) 
-				|| valSat(word.substring(indexOp + 2, wordLength-1), varList, values);
+			return valuation(word.substring(1, indexOp - 1), values) 
+				|| valuation(word.substring(indexOp + 2, wordLength-1), values);
 		} // Se a palavra word é da forma (w1 > w2)
 		else {
-			if (!valSat(word.substring(1, indexOp - 1), varList, values)){
+			if (!valuation(word.substring(1, indexOp - 1), values)){
 				return true;
-			} else if (valSat(word.substring(indexOp + 2, wordLength-1), varList, values)){
+			} else if (valuation(word.substring(indexOp + 2, wordLength-1), values)){
 				return true;
 			} else {
 				return false;
@@ -192,5 +188,17 @@ public class exprSolver {
 			}
 		}
 		return varList;
+	}
+
+	/*Cria um array onde cada posição está associada a uma letra do alfabeto, 
+		tornando direta a verificação do valor de uma variável*/
+	public static boolean[] fixValues(char[] varList, String[] values){
+		boolean[] newValues = new boolean [26];
+		for (int i = 0; i < varList.length; i++){
+			if(values[i].equals("1")){
+				newValues[varList[i]-65] = true;
+			}
+		}
+		return newValues;
 	}
 }
